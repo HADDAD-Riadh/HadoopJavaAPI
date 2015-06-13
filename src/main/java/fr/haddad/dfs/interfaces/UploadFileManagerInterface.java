c24cf0d9aef8af7969f17989ed468a8634813dda
Haddad <riadh.haddad.tn@gmail.com>
2015-06-13 14:17:48 +0200

    Update UploadFileManagerInterface.java

--- src/main/java/fr/haddad/dfs/interfaces/UploadFileManagerInterface.java
+++ src/main/java/fr/haddad/dfs/interfaces/UploadFileManagerInterface.java
@@ -1,12 +1,14 @@
 package fr.haddad.dfs.interfaces;
 
 /**
- * for the test of this API, I used a NameNode and three datanodes
- * that are placed on 4 machines, the flows are sent to NameNode 
+ * for the test of this API, I used 2 NameNodes and three datanodes
+ * that are placed on 5 machines, the flows are sent to the active NameNode that is chosen automatically  
  * and it manages communication with datanodes blocks in terms 
- * of management ..... .
+ * of management ..... .,user should indicate the address of the cluster in core-site.xml,and hdfs-core.xml.
+ * if client don't like to use high availability cluster ,we should indicate the hdfs url in config.properties file
+ * it is tested locally 
  * if you want to send a stream remotely, you just use a 
- * web service which receives a file and in using this API.
+ * web service which receives a file and in use this API.
  */
 import java.io.IOException;
 import java.net.URISyntaxException;
